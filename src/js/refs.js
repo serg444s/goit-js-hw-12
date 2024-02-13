854ba7030447cc03ca6d418c203fc2017d43a156
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-13 15:04:46 +0200

    css

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  form: document.querySelector('.user-form'),
+  formInput: document.querySelector('.form-input'),
+  btn: document.querySelector('.form-button'),
+  galleryList: document.querySelector('.gallery'),
+  container: document.querySelector('.container'),
+  loader: document.querySelector('.loader'),
+};
