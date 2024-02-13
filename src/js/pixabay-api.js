783202a5c5a9f8c3595122f2de4fd1e2eaba5f24
serg444s <143083342+serg444s@users.noreply.github.com>
2024-02-13 20:30:51 +0200

    async

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,33 +5,38 @@ import { makeGalleryItem } from './render-functions';
 import { onError } from './iziToasts';
 import axios from 'axios';
 
-export function onFormSubmit(event) {
+let count = 1;
+
+export async function onFormSubmit(event) {
   event.preventDefault();
   loaderOn();
   refs.galleryList.innerHTML = '';
-
   const userSearch = event.currentTarget.elements.input.value.trim();
-  fetchImg(userSearch).then(makeGalleryItem).catch(onError).finally(loaderOff);
-  refs.form.reset();
+  try {
+    const result = await fetchImg(userSearch);
+    const item = await makeGalleryItem(result);
+    return item;
+  } catch (error) {
+    onError;
+  } finally {
+    loaderOff();
+    refs.form.reset();
+  }
 }
 
-export function fetchImg(input) {
+export async function fetchImg(input) {
   const API_KEY = '42220995-e7901b62efa710cae16c4a0a7';
   axios.defaults.baseURL = 'https://pixabay.com/api/';
   const parameters = `q=${input}&image_type=photo&orientation=horizontal&safesearch=true`;
   const URL = `?key=${API_KEY}&${parameters}`;
-  let count = 1;
-  console.log(count);
 
-  return axios
-    .get(URL, {
-      params: {
-        per_page: 15,
-        page: count,
-        _limit: 15,
-      },
-    })
-    .then(response => {
-      return response.data;
-    });
+  const response = await axios.get(URL, {
+    params: {
+      per_page: 15,
+      page: count,
+      _limit: 15,
+    },
+  });
+
+  return response.data;
 }
